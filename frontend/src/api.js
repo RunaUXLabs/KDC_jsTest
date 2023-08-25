f8efb63c33fd9a0bc5c9caa80f2c586f9c39aa3c
RunaUXLabs <runa.uxlabs@gmail.com>
2023-08-25 16:18:17 +0900

    노드모듈 이동

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,6 @@
-const API_ENDPOINT =
+const API_ENDPOINT = 
+  // "http://192.168.0.51:8080";
+  // "http://127.0.0.1:8080";
   "https://rhdd0roxs5.execute-api.ap-northeast-2.amazonaws.com/dev";
 
 const api = {
